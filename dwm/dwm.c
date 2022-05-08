21ba765de2337b7dfd1d03b5e2c80fbfccdef7d8
DavMer <davmer@azod.de>
2022-05-08 18:36:41 +0200

    Reverting to 2f80cc6

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -1295,24 +1295,7 @@ propertynotify(XEvent *e)
 void
 quit(const Arg *arg)
 {
-	FILE *fd = NULL;
-	struct stat filestat;
-
-	if ((fd = fopen(lockfile, "r")) && stat(lockfile, &filestat) == 0) {
-		fclose(fd);
-
-		if (filestat.st_ctime <= time(NULL)-2)
-			remove(lockfile);
-	}
-
-	if ((fd = fopen(lockfile, "r")) != NULL) {
-		fclose(fd);
-		remove(lockfile);
-		running = 0;
-	} else {
-		if ((fd = fopen(lockfile, "a")) != NULL)
-			fclose(fd);
-	}
+	running = 0;
 }
 
 Monitor *
